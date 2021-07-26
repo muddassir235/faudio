ade8956ad80d862529c28cb14937c65cd0ec5549
Muddassir Ahmed Khan <muddassir.ahmed235@gmail.com>
2021-07-26 18:28:01 +0400

    Add more Download state tests. Add audio state mapping tests.

--- faudio/src/test/java/com/muddassir/faudio/downloads/testdownloadstates.kt
+++ faudio/src/test/java/com/muddassir/faudio/downloads/testdownloadstates.kt
@@ -46,12 +46,13 @@ class TestDownloadStates {
     }
 
     @Test
-    fun testDownloadItemHashing() {
+    fun testActualDownloadItemHashing() {
         val uriA = makeUri()
-        val otherUri = makeUri()
+        val uriB = uriA
+        val uriC = makeUri()
 
         val actualStateItemA = ActualDownloadItemState(uriA, 100.0f)
-        val actualStateItemB = ActualDownloadItemState(uriA, 50.0f)
+        val actualStateItemB = ActualDownloadItemState(uriB, 50.0f)
 
         val mapActual = mutableMapOf<ActualDownloadItemState, Boolean>()
         mapActual[actualStateItemA] = true
@@ -59,14 +60,20 @@ class TestDownloadStates {
 
         assertTrue(mapActual.size == 1)
 
-
-        val actualStateItemC = ActualDownloadItemState(otherUri, 100.0f)
+        val actualStateItemC = ActualDownloadItemState(uriC, 100.0f)
         mapActual[actualStateItemC] = true
 
         assertTrue(mapActual.size ==2)
+    }
+
+    @Test
+    fun testExpectDownloadItemHashing() {
+        val uriA = makeUri()
+        val uriB = uriA
+        val uriC = makeUri()
 
         val expectedStateItemA = ExpectedDownloadItemState(uriA)
-        val expectedStateItemB = ExpectedDownloadItemState(uriA)
+        val expectedStateItemB = ExpectedDownloadItemState(uriB)
 
         val mapExpected = mutableMapOf<ExpectedDownloadItemState, Boolean>()
         mapExpected[expectedStateItemA] = true
@@ -74,15 +81,40 @@ class TestDownloadStates {
 
         assertTrue(mapExpected.size == 1)
 
-
-        val expectedStateItemC = ExpectedDownloadItemState(otherUri)
+        val expectedStateItemC = ExpectedDownloadItemState(uriC)
         mapExpected[expectedStateItemC] = true
 
         assertTrue(mapExpected.size ==2)
     }
 
     @Test
-    fun testDownloadItemList() {
+    fun testActualDownloadItemListEquality() {
+        val uri = makeUri()
+
+        val actualStateItemA = ActualDownloadItemState(uri, 100.0f)
+        val actualStateItemB = ActualDownloadItemState(uri, 50.0f)
+
+        val actualStateItemListA = listOf(actualStateItemA)
+        val actualStateItemListB = listOf(actualStateItemB)
+
+        assertTrue(actualStateItemListA == actualStateItemListB)
+    }
+
+    @Test
+    fun testExpectedDownloadItemListEquality() {
+        val uri = makeUri()
+
+        val expectedStateItemA = ExpectedDownloadItemState(uri)
+        val expectedStateItemB = ExpectedDownloadItemState(uri)
+
+        val expectedStateItemListA = listOf(expectedStateItemA)
+        val expectedStateItemListB = listOf(expectedStateItemB)
+
+        assertTrue(expectedStateItemListA == expectedStateItemListB)
+    }
+
+    @Test
+    fun testActualExpectedDownloadItemListComparison() {
         val uriA = makeUri()
 
         val actualStateItemA = ActualDownloadItemState(uriA, 100.0f)
@@ -95,61 +127,134 @@ class TestDownloadStates {
     }
 
     @Test
-    fun testDownloadStates() {
+    fun testActualDownloadItemListInequality() {
         val uriA = makeUri()
-        val otherUri = makeUri()
+        val uriB = makeUri()
+
+        val actualStateItemA = ActualDownloadItemState(uriA, 100.0f)
+        val actualStateItemB = ActualDownloadItemState(uriB, 100.0f)
+
+        val actualStateItemListA = listOf(actualStateItemA)
+        val actualStateItemListB = listOf(actualStateItemB)
+
+        assertFalse(actualStateItemListA == actualStateItemListB)
+    }
+
+    @Test
+    fun testExpectedDownloadItemListInequality() {
+        val uriA = makeUri()
+        val uriB = makeUri()
+
+        val expectedStateItemA = ExpectedDownloadItemState(uriA)
+        val expectedStateItemB = ExpectedDownloadItemState(uriB)
+
+        val expectedStateItemListA = listOf(expectedStateItemA)
+        val expectedStateItemListB = listOf(expectedStateItemB)
+
+        assertFalse(expectedStateItemListA == expectedStateItemListB)
+    }
+
+    @Test
+    fun testActualExpectedDownloadItemListInequality() {
+        val uriA = makeUri()
+        val uriB = makeUri()
 
         val actualStateItem = ActualDownloadItemState(uriA, 100.0f)
-        val expectedStateItem = ExpectedDownloadItemState(uriA)
+        val expectedStateItem = ExpectedDownloadItemState(uriB)
 
-        val actualStateA = ActualDownloadState(listOf(actualStateItem), false)
-        val expectedStateA = ExpectedDownloadState(listOf(expectedStateItem), false)
+        val actualStateItemList = listOf(actualStateItem)
+        val expectedStateItemList = listOf(expectedStateItem)
 
-        val actualStateB = ActualDownloadState(listOf(actualStateItem), true)
-        val expectedStateB = ExpectedDownloadState(listOf(expectedStateItem), true)
+        assertFalse(actualStateItemList == expectedStateItemList)
+    }
 
+    @Test
+    fun testDownloadStates() {
+        // A
+        val uriA = makeUri()
+
+        val actualStateItemA = ActualDownloadItemState(uriA, 100.0f)
+        val expectedStateItemA = ExpectedDownloadItemState(uriA)
+
+        val actualStateA = ActualDownloadState(listOf(actualStateItemA), false)
+        val expectedStateA = ExpectedDownloadState(listOf(expectedStateItemA), false)
+
+        // B
+        val uriB = uriA
+
+        val actualStateItemB = ActualDownloadItemState(uriB, 100.0f)
+        val expectedStateItemB = ExpectedDownloadItemState(uriB)
+
+        val actualStateB = ActualDownloadState(listOf(actualStateItemB), true)
+        val expectedStateB = ExpectedDownloadState(listOf(expectedStateItemB), true)
+
+        // C
+        val uriC = makeUri()
+
+        val actualStateItemC = ActualDownloadItemState(uriC, 100.0f)
+        val expectedStateItemC = ExpectedDownloadItemState(uriC)
+
+        val actualStateC = ActualDownloadState(listOf(actualStateItemC), true)
+        val expectedStateC = ExpectedDownloadState(listOf(expectedStateItemC), true)
+
+        // Checks
         assertTrue(actualStateA.equals(expectedStateA))
         assertTrue(actualStateB.equals(expectedStateB))
         assertFalse(actualStateA == actualStateB)
         assertFalse(actualStateA.equals(expectedStateB))
         assertFalse(expectedStateA == expectedStateB)
         assertFalse(actualStateB.equals(expectedStateA))
-
-        val actualStateC = ActualDownloadState(listOf(
-            ActualDownloadItemState(otherUri, 100.0f)
-        ), true)
-        val expectedStateC = ExpectedDownloadState(listOf(
-            ExpectedDownloadItemState(otherUri)), true)
-
         assertFalse(actualStateB == actualStateC)
         assertFalse(expectedStateB == expectedStateC)
     }
 
     @Test
-    fun testDownloadHashing() {
+    fun testActualDownloadStateHashing() {
         val uriA = makeUri()
+        val uriB = uriA
+        val uriC = makeUri()
 
-        val actualStateItem = ActualDownloadItemState(uriA, 100.0f)
-        val expectedStateItem = ExpectedDownloadItemState(uriA)
+        val actualStateItemA = ActualDownloadItemState(uriA, 100.0f)
+        val actualStateItemB = ActualDownloadItemState(uriB, 100.0f)
+        val actualStateItemC = ActualDownloadItemState(uriC, 100.0f)
 
-        val actualStateA = ActualDownloadState(listOf(actualStateItem), false)
-        val actualStateB = ActualDownloadState(listOf(actualStateItem), true)
+        val actualStateA = ActualDownloadState(listOf(actualStateItemA), false)
+        val actualStateB = ActualDownloadState(listOf(actualStateItemB), true)
+        val actualStateC = ActualDownloadState(listOf(actualStateItemC), true)
+        val actualStateCAlt = ActualDownloadState(listOf(actualStateItemC), true)
 
         val mapActual = mutableMapOf<ActualDownloadState, Boolean>()
 
         mapActual[actualStateA] = true
         mapActual[actualStateB] = true
+        mapActual[actualStateC] = true
+        mapActual[actualStateCAlt] = true
 
-        assertTrue(mapActual.size == 2)
+        assertTrue(mapActual.size == 3)
+    }
 
-        val expectedStateA = ExpectedDownloadState(listOf(expectedStateItem), false)
-        val expectedStateB = ExpectedDownloadState(listOf(expectedStateItem), true)
+    @Test
+    fun testExpectedDownloadStateHashing() {
+        val uriA = makeUri()
+        val uriB = uriA
+        val uriC = makeUri()
+
+        val expectedStateItemA = ExpectedDownloadItemState(uriA)
+        val expectedStateItemB = ExpectedDownloadItemState(uriB)
+        val expectedStateItemC = ExpectedDownloadItemState(uriC)
+
+        val expectedStateA = ExpectedDownloadState(listOf(expectedStateItemA), false)
+        val expectedStateB = ExpectedDownloadState(listOf(expectedStateItemB), true)
+        val expectedStateC = ExpectedDownloadState(listOf(expectedStateItemC), true)
+        val expectedStateCAlt = ExpectedDownloadState(listOf(expectedStateItemC), true)
 
         val mapExpected = mutableMapOf<ExpectedDownloadState, Boolean>()
 
         mapExpected[expectedStateA] = true
         mapExpected[expectedStateB] = true
+        mapExpected[expectedStateC] = true
+        mapExpected[expectedStateCAlt] = true
 
-        assertTrue(mapExpected.size == 2)
+        assertTrue(mapExpected.size == 3)
     }
 }
--- faudio/src/test/java/com/muddassir/faudio/testaudiomappings.kt
+++ faudio/src/test/java/com/muddassir/faudio/testaudiomappings.kt
@@ -1,2 +1,35 @@
 package com.muddassir.faudio
 
+import android.net.Uri
+import junit.framework.TestCase.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.core.classloader.annotations.PowerMockIgnore
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(Uri::class)
+@PowerMockIgnore("jdk.internal.reflect.*")
+class TestAudioMappings {
+    @Test
+    fun testUriToMediaItem() {
+        val uri = makeUri()
+        val mediaItem = uriToMediaItem(uri)
+
+        assertTrue(uri == mediaItem.playbackProperties?.uri)
+    }
+
+    @Test
+    fun testActualAudioItemToExpectedAudioItem() {
+        val uri = makeUri()
+
+        val actualAudioItem = ActualAudioItem(uri, true, false, 50.0f)
+        val expectedAudioItem = actualAudioItemToExpectedAudioItem(actualAudioItem)
+
+        assertTrue(actualAudioItem.uri == expectedAudioItem.uri)
+        assertTrue(actualAudioItem.download == expectedAudioItem.download)
+
+        assertTrue(actualAudioItem.equals(expectedAudioItem))
+    }
+}
